497164a8d9d29f1b430ace577f950a598441799a
mar <gim.marcello@gmail.com>
2025-04-11 18:49:09 +0200

    changed: reformatting and comments.
    Adding missing in-comment documentation for all types/funcs, moving
    declarations around a tiny bit. Adding a couple more explicit error messages.

--- extract.go
+++ extract.go
@@ -10,8 +10,15 @@ import (
 	"modernc.org/cc/v4"
 )
 
+// A Context object holds the name/aggregate mappings for all parsed
+// aggregates within the source code under analysis. A pointer to an aggregate
+// may be present under multiple identifiers, which are the Context keys.
 type Context map[string]*Aggregate
 
+// A Layout object holds size/alignment/padding information with reference to
+// a field of an aggregate. If the field is an Aggregate type, then the
+// subAggregate slice is non-nil and contains the layout information for
+// each of its sub-fields.
 type Layout struct {
 	Field
 	size         int
@@ -20,6 +27,9 @@ type Layout struct {
 	subAggregate []Layout
 }
 
+// An AggregateMeta object holds the size and alignment data related to an
+// aggregate in general, and a list of the same layout information for each
+// of its fields-
 type AggregateMeta struct {
 	Size      int
 	Alignment int
@@ -33,100 +43,79 @@ var (
 )
 
 const (
-	includeErrMsg = `you are using an '#include' directive, but the tool does not 
-resolve include paths by default. Use the '-use-compiler' flag to force that
-behavior.`
-	compErrMsg = `you are attempting to use the system compiler through the 
-'-use-compiler' flag, but it could be that you do not have one installed or 
-that this tool cannot find it. The tool checks the CC environment variable, cc
-alias and gcc executable for a compiler and fails if no one works.`
+	includeErrMsg = `
+you are using an '#include' directive, but the tool does not resolve include 
+paths by default. Use the '-use-compiler' flag to force that behavior.`
+	compErrMsg = `
+you are attempting to use the system compiler through the '-use-compiler' 
+flag, but it could be that you do not have one installed or that this tool 
+cannot find it. The tool checks the CC environment variable, cc alias and gcc 
+executable for a compiler and fails if no one works.`
 )
 
-func (ctx Context) firstPass(fields []Field) ([]AggregateMeta, int, error) {
-	maxAlign := 0
-	resMetas := make([]AggregateMeta, 0, len(fields))
+// ExtractAggregates parses the passed C source code contained in `cont` to
+// produce a mapping of the aggregates in use within a context object instance.
+// On error, it returns a description of where the process failed.
+// You can specify a name together with the source code passed as input.
+//
+// Note that by default, this works without a compiler installation, but an
+// include path will be needed if any `#include` directives are used. In that
+// case, the `useCompiler` flag can be used.
+func ExtractAggregates(fname, cont string, useCompiler bool) (Context, error) {
+	config, err := cc.NewConfig(runtime.GOOS, runtime.GOARCH)
+	if err != nil {
+		return nil, fmt.Errorf("%w:\n%s Original error: \n\t%w", ErrConfig,
+			compErrMsg, err)
+	}
 
-	// First pass: evaluate the max alignment in the struct
-	for _, field := range fields {
-		switch field.(type) {
-		case Basic, Array:
-			agg, err := ctx.handleValueType(field)
+	srcs := []cc.Source{
+		{Name: "<predefined>", Value: config.Predefined},
+		{Name: "<builtin>", Value: cc.Builtin},
+		{Name: fname, Value: cont},
+	}
+
+	ast, err := cc.Translate(config, srcs)
+	if err != nil {
+		if strings.Contains(err.Error(), "include") {
+			return nil, fmt.Errorf("%w:%s\nOriginal error: \n\t%w", ErrParse,
+				includeErrMsg, err)
+		}
+		return nil, fmt.Errorf("%w: %w", ErrParse, err)
+	}
+
+	var ctx = make(Context)
+
+	// let us iterate over all declaration in the translation unit
+	for l := ast.TranslationUnit; l != nil; l = l.TranslationUnit {
+		extDecl := l.ExternalDeclaration
+
+		// we are only interested in ExternalDeclarationDecl...
+		if extDecl.Case != cc.ExternalDeclarationDecl {
+			continue
+		}
+
+		specifiers := extDecl.Declaration.DeclarationSpecifiers
+
+		// ...and specifically to Structs, Unions and Enums definitions
+		switch specifiers.Type().(type) {
+		case *cc.StructType, *cc.UnionType, *cc.EnumType:
+			aggregate, err := ParseAggregate(extDecl.Declaration)
 			if err != nil {
-				return nil, -1, err
+				return nil, err
 			}
 
-			resMetas = append(resMetas, agg)
-			if agg.Alignment > maxAlign {
-				maxAlign = agg.Alignment
-			}
-		case FuncPointer, Pointer:
-			resMetas = append(resMetas, AggregateMeta{
-				Size:      pointerSize,
-				Alignment: pointerAlign,
-			})
-
-			if pointerAlign > maxAlign {
-				maxAlign = pointerAlign
-			}
-		case EnumEntry:
-			resMetas = append(resMetas, AggregateMeta{
-				Size:      enumSize,
-				Alignment: enumAlign,
-			})
-
-			if enumAlign > maxAlign {
-				maxAlign = enumAlign
+			names := GetAggregateNames(aggregate)
+			for _, name := range names {
+				ctx[name] = aggregate
 			}
 		}
 	}
-	return resMetas, maxAlign, nil
+	return ctx, nil
 }
 
-func (ctx Context) handleValueType(field Field) (AggregateMeta, error) {
-	arrType, isArray := field.(Array)
-	fType := field.UnqualifiedType()
-
-	fMeta, isBase := TypeMap[fType]
-	if isBase {
-		size := fMeta.Size
-		if isArray {
-			size *= arrType.Elements
-		}
-
-		return AggregateMeta{
-			Size:      size,
-			Alignment: fMeta.Alignment,
-		}, nil
-	}
-
-	// Aggregate case
-	subMeta, err := ctx.resolveAggregate(fType)
-	if err != nil {
-		return AggregateMeta{}, err
-	}
-
-	if isArray {
-		subMeta.Size *= arrType.Elements
-	}
-
-	return subMeta, nil
-}
-
-func (ctx Context) resolveAggregate(aggType string) (AggregateMeta, error) {
-	// Let us check if this type is defined first
-	fAgg, isAggregate := ctx[aggType]
-	if !isAggregate {
-		return AggregateMeta{}, ErrSymbol
-	}
-
-	// If so, let us recursively resolve its alignment/size/padding
-	subMeta, err := ctx.ResolveMeta(fAgg.Name)
-	if err != nil {
-		return AggregateMeta{}, err
-	}
-	return subMeta, nil
-}
-
+// ResolveMeta attempts to get the alignment/size metadata for the aggregate
+// identified by name, within an initialized context. This will also
+// recursively compute said metadata for any inner aggregate.
 func (ctx Context) ResolveMeta(name string) (AggregateMeta, error) {
 	agg, ok := ctx[name]
 	if !ok {
@@ -218,6 +207,9 @@ func (ctx Context) ResolveMeta(name string) (AggregateMeta, error) {
 	}, nil
 }
 
+// Optimize applies the optimization algorithm for minimizing the padding in
+// C aggregates on the passed AggregateMeta, returning a new copy where its
+// field may have been re-ordered.
 func (ctx Context) Optimize(name string, meta AggregateMeta) (AggregateMeta, error) {
 	layout := make([]Layout, len(meta.Layout))
 	copy(layout, meta.Layout)
@@ -234,55 +226,97 @@ func (ctx Context) Optimize(name string, meta AggregateMeta) (AggregateMeta, err
 	return ctx.ResolveMeta(name)
 }
 
-func ExtractAggregates(fname, cont string, useCompiler bool) (Context, error) {
+// firstPass implementes, as the name suggests, the first pass in the
+// metadata resolution algorithm: it computes the max alignment for the
+// current aggregate, and handles any kind of field found, recursively
+// calling the resolution algorithm once more if it encounter another
+// aggregate.
+func (ctx Context) firstPass(fields []Field) ([]AggregateMeta, int, error) {
+	maxAlign := 0
+	resMetas := make([]AggregateMeta, 0, len(fields))
 
-	config, err := cc.NewConfig(runtime.GOOS, runtime.GOARCH)
-	if err != nil {
-		return nil, fmt.Errorf("%w:\n%s Original error: \n\t%w", ErrConfig,
-			compErrMsg, err)
-	}
-
-	srcs := []cc.Source{
-		{Name: "<predefined>", Value: config.Predefined},
-		{Name: "<builtin>", Value: cc.Builtin},
-		{Name: fname, Value: cont},
-	}
-
-	ast, err := cc.Translate(config, srcs)
-	if err != nil {
-		if strings.Contains(err.Error(), "include") {
-			return nil, fmt.Errorf("%w:\n%s Original error: \n\t%w", ErrParse,
-				includeErrMsg, err)
-		}
-		return nil, fmt.Errorf("%w: %w", ErrParse, err)
-	}
-
-	var ctx = make(Context)
-
-	// let us iterate over all declaration in the translation unit
-	for l := ast.TranslationUnit; l != nil; l = l.TranslationUnit {
-		extDecl := l.ExternalDeclaration
-
-		// we are only interested in ExternalDeclarationDecl...
-		if extDecl.Case != cc.ExternalDeclarationDecl {
-			continue
-		}
-
-		specifiers := extDecl.Declaration.DeclarationSpecifiers
-
-		// ...and specifically to Structs, Unions and Enums definitions
-		switch specifiers.Type().(type) {
-		case *cc.StructType, *cc.UnionType, *cc.EnumType:
-			aggregate, err := ParseAggregate(extDecl.Declaration)
+	// First pass: evaluate the max alignment in the struct
+	for _, field := range fields {
+		switch field.(type) {
+		case Basic, Array:
+			agg, err := ctx.handleValueType(field)
 			if err != nil {
-				return nil, err
+				return nil, -1, err
 			}
 
-			names := GetAggregateNames(aggregate)
-			for _, name := range names {
-				ctx[name] = aggregate
+			resMetas = append(resMetas, agg)
+			if agg.Alignment > maxAlign {
+				maxAlign = agg.Alignment
+			}
+		case FuncPointer, Pointer:
+			resMetas = append(resMetas, AggregateMeta{
+				Size:      pointerSize,
+				Alignment: pointerAlign,
+			})
+
+			if pointerAlign > maxAlign {
+				maxAlign = pointerAlign
+			}
+		case EnumEntry:
+			resMetas = append(resMetas, AggregateMeta{
+				Size:      enumSize,
+				Alignment: enumAlign,
+			})
+
+			if enumAlign > maxAlign {
+				maxAlign = enumAlign
 			}
 		}
 	}
-	return ctx, nil
+	return resMetas, maxAlign, nil
+}
+
+// handleValueType implements the value type (and array) handling strategy
+// for the metadata resolution algorithm. If the type is a primitive one, it
+// checks for its metadata within a lookup table, otherwise it attempts to
+// compute the aggregate metadata.
+func (ctx Context) handleValueType(field Field) (AggregateMeta, error) {
+	arrType, isArray := field.(Array)
+	fType := field.UnqualifiedType()
+
+	fMeta, isBase := TypeMap[fType]
+	if isBase {
+		size := fMeta.Size
+		if isArray {
+			size *= arrType.Elements
+		}
+
+		return AggregateMeta{
+			Size:      size,
+			Alignment: fMeta.Alignment,
+		}, nil
+	}
+
+	// Aggregate case
+	subMeta, err := ctx.resolveAggregate(fType)
+	if err != nil {
+		return AggregateMeta{}, err
+	}
+
+	if isArray {
+		subMeta.Size *= arrType.Elements
+	}
+
+	return subMeta, nil
+}
+
+// resolveAggregate tries to resolve the sub-aggregate passed by its type.
+func (ctx Context) resolveAggregate(aggType string) (AggregateMeta, error) {
+	// Let us check if this type is defined first
+	fAgg, isAggregate := ctx[aggType]
+	if !isAggregate {
+		return AggregateMeta{}, fmt.Errorf("%w: inner %s", ErrSymbol, aggType)
+	}
+
+	// If so, let us recursively resolve its alignment/size/padding
+	subMeta, err := ctx.ResolveMeta(fAgg.Name)
+	if err != nil {
+		return AggregateMeta{}, err
+	}
+	return subMeta, nil
 }
